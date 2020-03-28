3038781c6689b1eb596c713958fb86b1f5610635
Zach Rodis <zrodis@kbra.com>
2020-03-27 22:00:44 -0400

    test setup in index

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
 import { User } from './models/User';
 
-const testUser = new User({ id: 1, name: 'Dr. Boogaloo', age: 20 });
-console.log('testUser', testUser.data);
-console.log("testUser get 'name'", testUser.get('name'));
-console.log('change event added');
-testUser.on('change', event => {
-    console.log('trigger', event);
-    console.log("testUser 'name'", testUser.get('name'));
-});
+globalThis.testUser = new User({ id: 1, name: 'Dr. Boogaloo', age: 20 });
 
-testUser.trigger('change');
+const { testUser }: any = globalThis;
 
-console.log('set id', testUser.set({ name: 'dr. nobody' }));
+globalThis.testUserAll = () => {
+    console.log('::: testUser', testUser.data);
+    console.log("testUser get 'name'", testUser.get('name'));
+    console.log('change event added');
+    testUser.on('change', event => {
+        console.log('trigger', event);
+        console.log("testUser 'name'", testUser.get('name'));
+    });
+
+    testUser.trigger('change');
+    (async function() {
+        const response = await testUser.fetch();
+        console.log('testUser.fetch', response);
+
+        const responseSave = await testUser.save();
+        console.log('testUser.save', responseSave);
+        console.log('::: testUser', testUser.data);
+    })();
+};
